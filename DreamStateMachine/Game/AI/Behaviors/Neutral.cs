5827d964a8df73be131f858a2971f20c2ff194ee
connorclockwise <connorclockwise@gmail.com>
2014-10-05 15:30:27 -0400

    Merging weapons branch

---
+++ DreamStateMachine/Game/AI/Behaviors/Neutral.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using DreamStateMachine.Behaviors;
+
+namespace DreamStateMachine.Actions
+{
+    class Neutral:Behavior
+    {
+        Actor owner;
+        float nextWander;
+        Random random;
+        World world;
+
+        public Neutral(ActionList ownerList, Actor owner)
+        {
+            this.ownerList = ownerList;
+            this.owner = owner;
+            elapsed = 0;
+            duration = -1;
+            world = owner.world;
+            random = new Random();
+        }
+
+        override public void onStart()
+        {
+            //owner.setBodyAnimationFrame(0, 0);
+            nextWander = random.Next(6, 15);
+        }
+
+        override public void onEnd()
+        {
+
+        }
+
+        override public void update(float dt)
+        {
+            elapsed += dt;
+            if (elapsed > nextWander)
+            {
+                elapsed = 0;
+                nextWander = random.Next(3, 10);
+                Wander wander = new Wander(ownerList, owner);
+                if (!ownerList.has(wander))
+                {
+                    ownerList.pushFront(wander);
+                }
+            }
+        }
+    }
+}
---
+++ DreamStateMachine/Game/Events/PickupEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace DreamStateMachine
+{
+    class PickupEventArgs : EventArgs
+    {
+        public String itemClassName;
+
+        public PickupEventArgs(String itemClassName)
+            : base()
+        {
+            this.itemClassName = itemClassName;
+        }
+    }
+}
